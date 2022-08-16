cde049c34c48ce36f149db7227ef96a6713576bb
Pajsen9263 <42203569+Pajsen9263@users.noreply.github.com>
2022-08-16 17:18:55 +0200

    Custom path and error handling
    Fixed error handling if a user does not provide a URL. Added the path option, defualts to "./downloads"

--- index.js
+++ index.js
@@ -1,23 +1,32 @@
 const fs = require('fs');
 const axios = require('axios');
+let Location = ""
+function install(URL, path) {
+    if (URL === undefined || URL === null) {
+        throw new Error('URL is required');
+    }
+    if (path === undefined || path === null) {
+        {
+            Location = "./downloads/"
+        } else {
+            Location = path
+        }
+        axios
+            .get(URL)
+            .then(res => {
+                const host = res.request.host;
+                let charset = "abcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+                let text = "";
+                for (let i = 0; i < 8; i++)
+                    text += charset.charAt(Math.floor(Math.random() * charset.length));
+                fs.writeFile("./plugins/" + host.replace(".", "_") + "-" + text + '.txt', res.data, (err) => {
+                    if (err) throw err;
+                });
 
-function install(URL) {
-    axios
-        .get(URL)
-        .then(res => {
-            const host = res.request.host;
-            let charset = "abcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-            let text = "";
-            for (let i = 0; i < 8; i++)
-                text += charset.charAt(Math.floor(Math.random() * charset.length));
-            fs.writeFile("./plugins/" + host.replace(".", "_") + "-" + text + '.txt', res.data, (err) => {
-                if (err) throw err;
+            })
+            .catch(error => {
+                console.error(error);
             });
+    }
 
-        })
-        .catch(error => {
-            console.error(error);
-        });
-}
-
-module.exports = install;
+    module.exports = install;
