930e2a9feaa3a8698380ca6450e40fe2ea5aea95
Pajsen9263 <42203569+Pajsen9263@users.noreply.github.com>
2022-08-29 13:21:39 +0200

    File extention support (beta)
    Added file extension saving

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const fs = require('fs');
 const axios = require('axios');
 let Location = ""
-function install(URL, path) {
-    if (URL === undefined || URL === null) {
-        throw new Error('URL is required');
+function install(address, path) {
+    if (address === undefined || address === null) {
+        throw new Error('address is required');
     }
     if (path === undefined || path === null) {
 
@@ -12,21 +12,33 @@ function install(URL, path) {
         Location = path
     }
     axios
-        .get(URL)
+        .get(address)
         .then(res => {
+            const url = new URL(address);
+            const url_path = url.pathname;
+            const extension = url_path.split('.').pop();
             const host = res.request.host;
             let charset = "abcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
             let text = "";
             for (let i = 0; i < 8; i++)
                 text += charset.charAt(Math.floor(Math.random() * charset.length));
-            fs.writeFile(Location + host.replace(".", "_") + "-" + text + '.txt', res.data, (err) => {
-                if (err) throw err;
-            });
+            if (extension === null) {
+                fs.writeFile(Location + host.replace(".", "_") + "-" + text + '.txt', res.data, (err) => {
+                    if (err) throw err;
+                });
+            }
+            else {
+                fs.writeFile(Location + host.replace(".", "_") + "-" + text + '.' + extension, res.data, (err) => {
+                    if (err) throw err;
+                }
+                );
+            }
 
         })
         .catch(error => {
             console.error(error);
         });
 }
+
 
 module.exports = install;
